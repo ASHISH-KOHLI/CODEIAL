1163637ca716f2e2688ea559cce8de3c4a62b732
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-07-08 17:53:21 +0530

    added ejs in project

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 8000;
 // use express router
 app.use('/', require('./routes')); 
 
+//set up a view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port, function(err){
      if (err){
