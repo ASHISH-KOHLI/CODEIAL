d7f97a47d8aa214e6346370bb11e21db16fddb6e
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-07-09 17:12:10 +0530

    some changes

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const expressLayouts = require('express-ejs-layouts');
 
 
 app.use(express.static('./assets'));
+
 app.use(expressLayouts);
+
+// extract style and scripts from sub pages into the layout
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 
 
 // use express router
