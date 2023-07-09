3eb3547c492f4fe730585ee834c25af7e54b0b87
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-07-09 16:30:54 +0530

    installation of express ejs layout and creating layout.ejs file

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
+
 
 // use express router
 app.use('/', require('./routes')); 
