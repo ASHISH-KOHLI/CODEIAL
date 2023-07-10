d31aa7aff2ec087085d3d931a242a3243c05bc09
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-07-10 21:25:11 +0530

    create router for sign in and sign up

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users_controller');
+// const { route } = require('./users');
 
 router.get('/profile', userController.profile);
+
+
+router.get('/sign_up',userController.signUp);
+router.get('/sign_ip',userController.signIn);
+
+
+
 module.exports = router;
