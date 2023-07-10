0bf7cd73672ef5bd9f7bf4f74650a7c07f6b3e01
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-07-10 21:13:33 +0530

    create a controller for user sign_up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,9 @@
 module.exports.profile = function(req, res){
     res.end(`<h1>User Profile</h1>`);
 }
+
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up',{
+        title:"Codeial | signUp"
+    })
+}
