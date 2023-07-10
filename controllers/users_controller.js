963fc0214d4364f9f057b5298d61ef3851cbbeef
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-07-10 21:18:55 +0530

    render the sign in page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,8 +2,20 @@ module.exports.profile = function(req, res){
     res.end(`<h1>User Profile</h1>`);
 }
 
+// render the sign up page
+
 module.exports.signUp = function(req, res){
     return res.render('user_sign_up',{
         title:"Codeial | signUp"
+    })
+}
+
+
+
+// render the sign in page
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in', {
+        title: "codeial | Sign In"
     })
 }
