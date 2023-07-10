caf9c815a862d0b2840412268ee942808c9b39d1
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-07-10 21:01:03 +0530

    added user Schema

--- models/user.js
+++ models/user.js
@@ -2,5 +2,17 @@ const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
-
+    email: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type:String,
+        required:true
+    }
 })
