4660e938b375b5e352a4e0ba26ef5657767bcb19
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-07-11 13:12:34 +0530

    some changes

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,9 @@
 module.exports.profile = function(req, res){
-    res.end(`<h1>User Profile</h1>`);
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
 }
+
 
 // render the sign up page
 
@@ -26,6 +29,8 @@ module.exports.create =function(req, res){
     // todo later
 }
 
+
+// get the sign in and create a session for the user
 module.exports.createSession =function(req, res){
     // todo later
 }
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
-console.log("router loaded")
+console.log('router loaded');
 
-router.get('/',homeController.home)
+router.get('/',homeController.home);
 
 router.use('/users', require('./users'));
 
