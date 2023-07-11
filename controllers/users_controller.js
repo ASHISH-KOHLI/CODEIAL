14b0a0435185cb8b7f1369e980bbe61bfad68de4
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-07-11 17:36:05 +0530

    passport

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,95 +1,52 @@
-const User = require('../models/user')
+const User = require('../models/user');
+
 
 module.exports.profile = function(req, res){
-  if(req.cookies.user_id){
-    User.findById(req.cookies.user_id, function(err, user){
-        if(user){
-            return res.render('user_profile', {
-                title: 'User Profile'
-            })
-        } else{
-            return res.redirect('/users/sign-in');
-          }
-        
-    });
-  }
-  else{
-    return res.redirect('/users/sign-in');
-  }
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
 }
 
 
 // render the sign up page
-
 module.exports.signUp = function(req, res){
-    return res.render('user_sign_up',{
-        title:"Codeial | signUp"
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
     })
 }
 
 
-
 // render the sign in page
-
 module.exports.signIn = function(req, res){
     return res.render('user_sign_in', {
-        title: "codeial | Sign In"
+        title: "Codeial | Sign In"
     })
 }
 
 // get the sign up data
-
-module.exports.create =function(req, res){
-    if(req.body.password != req.body.confirm_password){
+module.exports.create = function(req, res){
+    if (req.body.password != req.body.confirm_password){
         return res.redirect('back');
     }
+
     User.findOne({email: req.body.email}, function(err, user){
-      if(err){console.log('error in finding user in singing up'); return}  
+        if(err){console.log('error in finding user in signing up'); return}
 
-      if (!user){
-        User.create(req.body, function(err, user){
-            if(err){console.log('error in creating  user while singing up'); return}  
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up'); return}
 
-            return res.redirect('/user/sign-in');
-        })
-      }else{
-        return res.redirect('back');
-      }
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
 
     });
 }
 
 
-// get the sign in and create a session for the user
-module.exports.createSession =function(req, res){
-    
-    
-//    steps to authnticate
-    // find the user 
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in singing in'); return}  
-        // handel user found
-        if(user){
-            // handle password which dont match
-            if(user.password != req.body.password){
-            return res.redirect('back');
-                
-            }
-            // handle session creation
-           
-            res.cookie('user_id',user.id);
-            return res.redirect('/users/profile');
-            
-
-        }else{
-            // handle user not found
-            return res.redirect('back');
-        }
-
-    });
-
-
-
-
-
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    // TODO later
 }
