4077f3d5dbba1b821f88e687cbf7df1a06f9a431
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-07-14 12:33:31 +0530

    b

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-
+const MongoStore= require('connect-mongo')(session);
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -27,7 +27,7 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
+// mongo store is used to store the session cookie
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
@@ -36,7 +36,17 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
+    },
+ Store: new MongoStore(
+    {
+   
+      mongConnection: db  ,
+      autoRemove:'disabled'
+    },
+    function(err){
+        console.log(err || 'connect-mongodb setup ok');
     }
+ )
 }));
 
 app.use(passport.initialize());
