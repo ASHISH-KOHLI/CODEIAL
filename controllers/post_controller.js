f859acc39ce29e539adc3f4499bf3167d3d43625
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-07-31 21:29:35 +0530

    post controller, route created

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,3 +1,16 @@
-module.exports.myPost = function(req,res){
-    res.end('<h1>Hellow post</H1>')
+const Post = require('../models/post')
+
+module.exports.create = function(req,res){
+   Post.create({
+       content:req.body.content,
+       user:req.user._id
+   },{
+    function(err,post){
+        if(err){
+            console.log('error in creationg a post');
+            return;
+        }
+        return res.redirect('back');
+    }
+   })
 }
--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const postController= require('../controllers/post_controller')
-router.get('/myPost',postController.myPost)
+router.post('/create',postController.create)
 
 
 
