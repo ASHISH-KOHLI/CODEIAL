0e453fe1acf0bdbc5f1fcc8267f3a8f23c9c9909
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-08-01 15:55:41 +0530

    show post in the index page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,23 @@
+const Post = require('../models/post');
+
 // a controller is a set of different actions
 
-module.exports.home = function(req,res){
-    return res.render('home',{
-         title:'home'
-    })
-}
+// Assuming you have already imported the required modules and models
+
+module.exports.home = async function(req, res) {
+    try {
+        const posts = await Post.find({}).populate('user').exec();
+
+        return res.render('home', {
+            title: 'home',
+            posts: posts
+        });
+    } catch (err) {
+        // Handle any potential errors here
+        console.error(err);
+        return res.status(500).send('Internal Server Error');
+    }
+};
+
 
 // module.exports.actionName = function(req,res){}
