28472473b637206e4661ea75fb39547cc3f498ff
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-08-02 14:22:30 +0530

    deleting a post (Authorized

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post')
+const Comment = require('../models/comment')
 
 module.exports.create = function (req, res) {
     Post.create({
@@ -12,4 +13,49 @@ module.exports.create = function (req, res) {
         console.log('Error in creating a post:', err);
         return res.status(500).send('Internal Server Error');
     });
+};
+
+
+
+
+// module.exports.destroy = function(req, res){
+//    Post.findById(req.params.id, function(err, post){
+// //.id means converting the object id into strings
+//     if(post.user == req.user.id){
+//         post.remove();
+
+//         Comment.deleteMany({post: req.params.id},function(err){
+//                return res.redirect('back');
+//         });
+//     }else{
+//         return res.redirect('back');
+//     }
+//    });
+// }
+
+
+module.exports.destroy = function (req, res) {
+    Post.findById(req.params.id)
+        .exec()
+        .then(post => {
+            if (!post) {
+                return res.redirect('back');
+            }
+
+            if (post.user.toString() !== req.user.id) {
+                return res.redirect('back');
+            }
+
+            return Promise.all([
+                post.deleteOne(), // or post.deleteMany() for multiple posts
+                Comment.deleteMany({ post: req.params.id }).exec()
+            ]);
+        })
+        .then(() => {
+            return res.redirect('back');
+        })
+        .catch(err => {
+            console.log('Error deleting post:', err);
+            return res.redirect('back');
+        });
 };
