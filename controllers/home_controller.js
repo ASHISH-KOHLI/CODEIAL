2d85dbee375366ce327cfb74179ad1eb7574c979
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-08-02 13:23:05 +0530

    Nest Population :: Display Comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,22 +2,29 @@ const Post = require('../models/post');
 
 // a controller is a set of different actions
 
-// Assuming you have already imported the required modules and models
 
 module.exports.home = async function(req, res) {
     try {
-        const posts = await Post.find({}).populate('user').exec();
+        const posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
+            })
+            .exec();
 
         return res.render('home', {
-            title: 'home',
+            title: 'Codeial | Home',
             posts: posts
         });
     } catch (err) {
-        // Handle any potential errors here
-        console.error(err);
-        return res.status(500).send('Internal Server Error');
+        console.log('Error fetching posts:', err);
+        return res.redirect('/');
     }
 };
+
 
 
 // module.exports.actionName = function(req,res){}
