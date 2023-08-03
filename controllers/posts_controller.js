a4047a6d4a80f74c4873b574246bf253b03c1d80
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-08-03 22:48:42 +0530

    async await

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,20 +1,32 @@
 const Post = require('../models/post')
 const Comment = require('../models/comment')
 
-module.exports.create = function (req, res) {
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    })
-    .then((post) => {
+// module.exports.create = function (req, res) {
+//     Post.create({
+//         content: req.body.content,
+//         user: req.user._id
+//     })
+//     .then((post) => {
+//         return res.redirect('back');
+//     })
+//     .catch((err) => {
+//         console.log('Error in creating a post:', err);
+//         return res.status(500).send('Internal Server Error');
+//     });
+// };
+
+module.exports.create = async function (req, res) {
+    try {
+        const post = await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
         return res.redirect('back');
-    })
-    .catch((err) => {
+    } catch (err) {
         console.log('Error in creating a post:', err);
         return res.status(500).send('Internal Server Error');
-    });
+    }
 };
-
 
 
 
