6892bd8554b6afd8445133119e09ea9b7edf8650
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-08-04 22:39:37 +0530

    creating a post :: Reciveing Data

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -11,7 +11,8 @@
             url:'/posts/create',
             data:newPostForm.serialize(),
             success: function(data){
-              console.log(data);
+             let newPost= newPostDom(data.data.post);
+             $('#posts-list-container>ul').prepend(newPost);
             },error: function(error){
                 console.log(error.responseText);
             }
@@ -19,6 +20,38 @@
     });
    }
 //    method to create a post in Dom
+let newPostDom = function(post){
+    return $(`<li id="post-${ post._id }">
+    <p>
+            <small>
+                <a class="delete-post-button" href="/posts/destroy/${post._id }">X</a>
+            </small>
+        
+         ${post.content }
+        <br>
+        <small>
+            ${post.user.name}  
+        </small>
+    </p>
+    <div class="post-comments">
+      
+          <form action="/comments/create" method="POST">
+              <input type="text" name="content" placeholder="Write a comment..." required>
+              <input type="hidden" name="post" value="${post._id }">
+              <input type="submit" value="Comment">
+          </form>
+ 
+      <div class="post-comments-list">
+        <ul id="post-comments-${post._id }">
+            
+        </ul>
+    </div>
+
+
+  </div>
+</li>    `)
+}
+ 
    createPost();
 }
 
