7f9c90888450a430fb886ec0ce2f04ebf29e263a
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-08-04 21:40:41 +0530

    creating a post ::sending data

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -21,6 +21,15 @@ module.exports.create = async function (req, res) {
             content: req.body.content,
             user: req.user._id
         });
+
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post:post
+                },
+                message:"post created"
+            });
+        }
         req.flash('success','post published')
         return res.redirect('back');
     } catch (err) {
