ed45b1e95332c623c48c23b9bb2cf596074f8eb0
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-08-05 12:50:47 +0530

    consiguring multer for user Avatar

--- models/user.js
+++ models/user.js
@@ -15,10 +15,23 @@ const userSchema = new mongoose.Schema({
     },name:{
         type: String,
         require:true
+    },avatar:{    
+        type:String 
     }
 },{
     timestamps:true
 });
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname,'..',AVATAR_PATH))
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+
 
 const User = mongoose.model('User',userSchema);
 
