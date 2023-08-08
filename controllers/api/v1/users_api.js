e1b20966a496619f1429df2b0d3b2d9ee49689a0
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-08-08 18:03:41 +0530

    autherization and authentication

--- controllers/api/v1/users_api.js
+++ controllers/api/v1/users_api.js
@@ -16,7 +16,7 @@ module.exports.createSession = async function (req, res) {
     return res.json(200,{
         message: 'siign in sucessful here is your token,please leep it safe ',
         data: {
-            token: jwt.sign(user.toJSON(),'codeial',{expiresIn: '1000'})
+            token: jwt.sign(user.toJSON(),'codeial',{expiresIn: '10000'})
         }
      })
 
