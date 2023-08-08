fa71546485cf656a0c13dfc224a1b2381ee3b654
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-08-08 13:16:21 +0530

    playing with API

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,50 @@
-module.exports.index = function(req,res){
+const Post = require("../../../models/post")
+const Comment = require("../../../models/comment")
+module.exports.index = async function(req,res){
+   
+    const posts = await Post.find({})
+    .sort('-createdAt')
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    });
+   
+   
+   
     return res.json(200,{
         message:"list of posts ",
-        posts:[]
+        posts:posts 
     })
 }
+
+
+
+module.exports.destroy = async function(req, res) {
+    try {
+        const postId = req.params.id;
+
+        // Delete the post using deleteOne
+        const deleteResult = await Post.deleteOne({ _id: postId });
+
+        if (deleteResult.deletedCount === 0) {
+            return res.status(404).json({
+                message: 'Post not found'
+            });
+        }
+
+        // Delete associated comments
+        await Comment.deleteMany({ post: postId });
+
+        return res.status(200).json({
+            message: 'Post and associated comments deleted successfully'
+        });
+    } catch (err) {
+        console.log('Error:', err);
+        return res.status(500).json({
+            message: 'Internal Server Error'
+        });
+    }
+};
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -6,4 +6,5 @@ const postsApi = require("../../../controllers/api/v1/posts_api")
 
 
 router.get('/',postsApi.index);
+router.delete('/:id', postsApi.destroy);
 module.exports=router;
