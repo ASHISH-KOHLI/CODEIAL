67cc4a19d217f3b594851096249e4c485afb9506
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-08-09 20:41:46 +0530

    added usign auth

--- config/passport-google-oauth2-strategy.js
+++ config/passport-google-oauth2-strategy.js
@@ -4,33 +4,39 @@ const crypto = require('crypto');
 const User = require('../models/user');
 
 
-passport.use(new googleStrategy({
-    clientID:"867126299882-gppqifndi5p7vtghjm0rsnu8egp8s1kt.apps.googleusercontent.com",
-    clientSecret:"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",
-    callbackURL:"http://localhost:8000/users/auth/google/callback",        
-},
+// passport.use(new googleStrategy({
+//     clientID:"867126299882-gppqifndi5p7vtghjm0rsnu8egp8s1kt.apps.googleusercontent.com",
+//     clientSecret:"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",
+//     callbackURL:"http://localhost:8000/users/auth/google/callback",        
+// },
 
-function(accessToken, refreshToken, profile, done){
-    User.findOne({email:profile.emails[0].value}).exec(function(err,user){
-        if(err){console.log('error in google strategy-passport',err); return}
+// function (accessToken, refreshToken, profile, done) {
+//     User.findOne({ email: profile.emails[0].value })
+//     .exec()
+//     .then(user => {
+//         if (user) {
+//             return done(null, user);
+//         } else {
+//             return User.create({
+//                 name: profile.displayName,
+//                 email: profile.emails[0].value,
+//                 password: crypto.randomBytes(20).toString('hex')
+//             });
+            
+//         }
+//     })
+//     .then(newUser => {
+//         // Handle the newly created user if necessary
+//         // and call done(null, newUser) if needed.
+//     })
+//     .catch(err => {
+//         console.log('error in google strategy-passport', err);
+//         return done(err);
+//         });
+// }
 
-        console.log(profile);
 
-        if(user){
-            return done(null,user);
-        }else{
-            User.create({
-                name:profile.displayName,
-                email:profile.emails[0].value,
-                password:crypto.randomBytes(20).toString('hex')
-            },function(err,user){
-                if(err){console.log('error in google strategy-passport',err); return}
-            })
-        }
-    })
-}
-
-))
+// ))
 
 
 
@@ -47,7 +53,7 @@ function(accessToken, refreshToken, profile, done){
 // function(accessToken, refreshToken, profile, done){
 //     User.findOne({email:profile.emails[0].value}).exec(function(err,user){
 //         if(err){console.log('error in google strategy-passport',err); return}
-
+        // console.log(accessToken,refershToken);
 //         console.log(profile);
 
 //         if(user){
