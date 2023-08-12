6521837074647fd2e44bdf38e04324740da64319
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-08-12 20:40:21 +0530

    some errors

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,36 +1,8 @@
 const Post = require('../models/post');
 const User = require('../models/user');
-// a controller is a set of different actions
 
 
-// module.exports.home = async function (req, res) {
-//     try {
-//         const posts = await Post.find({})
-//             .sort('-createdAt')
-//             .populate('user')
-//             .populate({
-//                 path: 'comments',
-//                 populate: {
-//                     path: 'user'
-//                 },
-//                 populate:{
-//                     path:'likes'
-//                 }
-//             })
-//             .exec();
 
-//         const users = await User.find({}).exec();
-
-//         return res.render('home', {
-//             title: 'Codeial | Home',
-//             posts: posts,
-//             all_users: users
-//         });
-//     } catch (err) {
-//         console.log('Error fetching data:', err);
-//         return res.redirect('/');
-//     }
-// };
 module.exports.home = async function(req, res){
 
     try{
@@ -38,21 +10,15 @@ module.exports.home = async function(req, res){
         let posts = await Post.find({})
         .sort('-createdAt')
         .populate('user')
-        .populate ({
+        .populate({
             path: 'comments',
-            populate:
-            {
+            populate: {
                 path: 'user'
-            }
-        })
-        .populate
-        ({
-            path: 'comments',
-            populate:
-            {
+            },
+            populate: {
                 path: 'likes'
             }
-        })
+        }).populate('comments')
         .populate('likes');
 
     
@@ -71,5 +37,12 @@ module.exports.home = async function(req, res){
    
 }
 
+// module.exports.actionName = function(req, res){}
 
-// module.exports.actionName = function(req,res){}
+
+// using then
+// Post.find({}).populate('comments').then(function());
+
+// let posts = Post.find({}).populate('comments').exec();
+
+// posts.then()
--- routes/likes.js
+++ routes/likes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router();
 const likesController = require('../controllers/likes_controller');
 
-router.post('/toggle', likesController.toogleLike);
+router.post('/toggle', likesController.toggleLike);
 
 module.exports = router;
 
