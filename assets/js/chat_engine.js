0f771c9aa5057eca283dae3da7a40354241e1e8c
ASHISH-KOHLI <kohli14ashish@gmail.com>
2023-08-14 21:47:30 +0530

    create and join chat

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -11,8 +11,19 @@ class ChatEngine{
   }
 
   connectionHandler(){
+    let self = this;
+
     this.socket.on('connect',function(){
         console.log('connection established using sockets .. ');
-    })
+      
+        self.socket.emit('join_room', {
+            user_email:self.userEmail,
+            chatroom:'codeal'
+        });
+        self.socket.on('user_joined', function(data){
+            console.log('a user joined',data)
+        })
+    });
+    
   }
 }
--- config/chat_scokets.js
+++ config/chat_scokets.js
@@ -6,6 +6,12 @@ module.exports.chatSockets =function(socketServer){
 
         socket.on('disconnect',function(){
             console.log('socket disconnected !'); 
+        });
+        socket.on('join_room',function(data){
+            console.log('joining request rec.', data);
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user joined, data');
         })
     })
 
